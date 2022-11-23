7370e4f5c8473bbf0519e9cba514b2f783c1a30d
B1ackSuit <ean969648839@gmail.com>
2022-11-23 17:20:06 +0800

    Initial commit

---
+++ src/main/java/cn/ean/oa_restart/OaRestartApplication.java
@@ -0,0 +1,13 @@
+package cn.ean.oa_restart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OaRestartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OaRestartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cn/ean/oa_restart/OaRestartApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.ean.oa_restart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OaRestartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
