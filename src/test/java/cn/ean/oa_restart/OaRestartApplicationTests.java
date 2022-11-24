76b06e2b8fdee31fc6772e6b5668f51396868955
B1ackSuit <ean969648839@gmail.com>
2022-11-24 14:48:53 +0800

    AutoGenerator之前所有文件

--- src/test/java/cn/ean/oa_restart/OaRestartApplicationTests.java
+++ src/test/java/cn/ean/oa_restart/OaRestartApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class OaRestartApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
