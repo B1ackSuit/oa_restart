9d3a25eb4f99fe3b4a1783902475929c7acfe346
B1ackSuit <ean969648839@gmail.com>
2022-11-24 15:11:22 +0800

    others file
    others file

---
+++ oa_emp/src/test/java/cn/ean/oa_emp/OaEmpApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.ean.oa_emp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OaEmpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
